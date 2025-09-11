aac31139bde75890c35928c6e2a3718b0db30dd1
Evgeny Makarov <zhen.makarov2077@gmail.com>
2025-09-11 15:16:04 +0300

    server works

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"FinalTaskForGO/pkg/api"
+	"FinalTaskForGO/pkg/server"
 	"log"
-	"net/http"
 	"os"
 )
 
-const defaultDir = "./web"
+const defaultDBfile = "scheduler.db"
 
 func main() {
-	webDir := os.Getenv("TODO_WEB_DIR")
-	if webDir == "" {
-		webDir = defaultDir
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = defaultDBfile
 	}
-	if _, err := os.Stat(webDir); os.IsNotExist(err) {
-		log.Fatalf("Web directory does not exist")
-	}
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
 
-	err := http.ListenAndServe(":7540", nil)
-	if err != nil {
-		log.Fatal("Server crashed", err)
+	if err := db.Init(dbFile); err != nil {
+		log.Fatal("DB is not init")
 	}
+	defer db.GetDB().Close()
+	api.Init()
+	server.Run()
+
 }
---
+++ pkg/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	pathToWebDir = "./web"
+	defaultPort  = "7540"
+)
+
+func Run() {
+	webDir := os.Getenv("TODO_WEB_DIR")
+	if webDir == "" {
+		webDir = pathToWebDir
+	}
+	if _, err := os.Stat(webDir); os.IsNotExist(err) {
+		log.Fatal("dir WEB can't find: ", err)
+	}
+
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("Server crashed err: ", err)
+	}
+
+}
